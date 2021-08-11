02388a7f61d84261603dde060ecf58b8752a76ac
nachesv <ignaciosanchezvilar@gmail.com>
2021-08-10 23:45:14 -0300

    clase commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import express from 'express';
+import routerApi from './routes/api.js';
 
-let productos = [
-    {
-      id: 1,
-      nombre: 'Escuadra',
-      precio: 200,
-    },
-    {
-      id: 2,
-      nombre: 'Transportador',
-      precio: 50,
-    },
-  ];
+
 
 const app = express();
 const puerto = 8080;
@@ -19,70 +9,31 @@ const server = app.listen(puerto, () =>
   console.log('Server up en puertoo', puerto)
 );
 
-app.get('/app/productos/listar', (req, res) => {
-    /**Opcionalmente podemos agregar queryes para hacer busquedas distintas y no traer todo*/
-    const filtroPrecio = req.query.price;
-    let data;
-  
-    console.log(filtroPrecio);
-  
-    if (filtroPrecio)
-      data = productos.filter(
-        (aProduct) => aProduct.precio >= Number(filtroPrecio)
-      );
-    else data = productos;
-  
-    res.json({
-      data,
-    });
-  });
+server.on('error', (err) => {
+  console.log('ERROR ATAJADO', err);
+});
 
-  app.get('/app/productos/:id', (req, res) => {
-    /**Opcionalmente podemos agregar queryes para hacer busquedas distintas y no traer todo*/
-    const filtroId = req.query.id;
-    let data;
-  
-    console.log(filtroPrecio);
-  
-    if (filtroPrecio)
-      data = productos.filter(
-        (aProduct) => aProduct.id = Number(filtroPrecio)
-      );
-    else data = productos;
-  
-    res.json({
-      data,
-    });
-  });
+
+const layoutDirPath = path.resolve(__dirname, '../views/layouts');
+const defaultLayerPth = path.resolve(__dirname, '../views/layouts/index.hbs');
+
+const partialDirPath = path.resolve(__dirname, '../views/partials');
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  handlebars({
+    layoutsDir: layoutDirPath,
+    extname: 'hbs',
+    defaultLayout: defaultLayerPth,
+    partialsDir: partialDirPath,
+  })
+);
+
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.post('/api/productos/guardar/', (req, res) => {
-  const body = req.body;
-  console.log(body);
 
-  /** Valido que la info que me mandaron este OK, sino respondo con 400 */
-  if (
-    !body.nombre ||
-    !body.precio ||
-    typeof body.nombre != 'string' ||
-    typeof body.precio != 'number'
-  ) {
-    return res.status(400).json({
-      msg: 'Necesito en el body el nombre (string) y el precio (number)',
-    });
-  }
+app.use('/api/productos', routerApi);
 
-  const nuevoProducto = {
-    id: productos.length + 1,
-    nombre: body.nombre,
-    precio: body.precio,
-  };
-
-  productos.push(nuevoProducto);
-
-  /**Estado 201: Objeto creado correctamente */
-  res.status(201).json({
-    data: nuevoProducto,
-  });
-});
---
+++ src/router/api.js
@@ -0,0 +1,125 @@
+import express from 'express';
+
+
+
+let productos = [
+    {
+      id: 1,
+      nombre: 'Escuadra',
+      precio: 200,
+      thumbnail: ''
+    },
+    {
+      id: 2,
+      nombre: 'Transportador',
+      precio: 50,
+      thumbnail: ''
+    },
+  ];
+
+const router = express.Router();
+
+router.get('/listar', (req, res) => {
+    /**Opcionalmente podemos agregar queryes para hacer busquedas distintas y no traer todo*/
+    const filtroPrecio = req.query.price;
+    let data;
+  
+    console.log(filtroPrecio);
+  
+    if (filtroPrecio)
+      data = productos.filter(
+        (aProduct) => aProduct.precio >= Number(filtroPrecio)
+      );
+    else data = productos;
+  
+    res.json({
+      data,
+    });
+  });
+
+  app.get('/:id', (req, res) => {
+    /**Opcionalmente podemos agregar queryes para hacer busquedas distintas y no traer todo*/
+    const filtroId = req.query.id;
+    let data;
+  
+    console.log(filtroPrecio);
+  
+    if (filtroPrecio)
+      data = productos.filter(
+        (aProduct) => aProduct.id = Number(filtroPrecio)
+      );
+    else data = productos;
+  
+    res.json({
+      data,
+    });
+  });
+
+
+router.post('/guardar', (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  /** Valido que la info que me mandaron este OK, sino respondo con 400 */
+  if (
+    !body.nombre ||
+    !body.precio ||
+    typeof body.nombre != 'string' ||
+    typeof body.precio != 'number'
+  ) {
+    return res.status(400).json({
+      msg: 'Necesito en el body el nombre (string) y el precio (number)',
+    });
+  }
+
+  const nuevoProducto = {
+    id: productos.length + 1,
+    nombre: body.nombre,
+    precio: body.precio,
+    thumbnail: body.thumbnail
+  };
+
+  productos.push(nuevoProducto);
+
+  /**Estado 201: Objeto creado correctamente */
+  res.status(201).json({
+    data: nuevoProducto,
+  });
+});
+
+router.put('/actualizar/:id', (req,res) => {
+
+    const body = req.body;
+    const productoActualizar = productos.filter(producto => producto.id = req.query.id)
+
+    productoActualizar = {
+      id: body.id,
+      nombre: body.nombre,
+      precio: body.precio,
+      thumbnail: body.thumbnail
+    }
+
+    productos = productos.filter(producto => producto.id != req.query.id)
+    productos.push(productoActualizar)
+
+
+    res.json({
+        nuevoProducto
+    })
+})
+
+router.delete('/api/productos/borrar/:id', (req,res) => {
+
+  const body = req.body;
+  
+
+  const productoBorrado = productos.filter(producto => producto.id = req.query.id)
+  productos = productos.filter(producto => producto.id != req.query.id)
+
+
+  res.json({
+    productoBorrado
+  })
+})
+
+export default router;
